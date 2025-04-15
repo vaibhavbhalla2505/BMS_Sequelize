d300eec7aa56f82de43159e5d42ba439a28078c5
vaibhav bhalla <vaibhav.bhalla@sourcefuse.com>
2025-04-15 11:03:28 +0530

    streaming perform on adding bulk data

--- src/controller/bookController.ts
+++ src/controller/bookController.ts
@@ -1,7 +1,12 @@
 import { RequestHandler } from "express";
 import { Book } from "../model/bookModel.js";
 import { sequelize } from "../config/dbConnect.js";
+import { Author } from "../model/authorModel.js";
+import { Category } from "../model/categoryModel.js";
+import { Readable } from "stream";
+import EventEmitter from "events";
 
+const eventEmitter=new EventEmitter();
 export const createBook:RequestHandler = async(req,res)=>{
     const transaction=await sequelize.transaction();
     try {
@@ -15,6 +20,17 @@ export const createBook:RequestHandler = async(req,res)=>{
             });
             return;
         }
+        //check if authors exists
+        const authorExists = await Author.findByPk(author_id);
+          if (!authorExists) {
+            throw new Error(`Author with ID ${author_id} not found`);
+          }
+
+          // Check if category exists
+          const categoryExists = await Category.findByPk(category_id);
+          if (!categoryExists) {
+            throw new Error(`Category with ID ${category_id} not found`);
+          }
         const book = await Book.create({title,publication_date,price,author_id,category_id,isbn} as any,{transaction});
 
         await transaction.commit();
@@ -125,3 +141,43 @@ export const deleteBookDetails:RequestHandler = async(req,res)=>{
         })
     }
 }
+
+export const bulkBook:RequestHandler = async (req,res) => {
+    const books = req.body; 
+    if (!Array.isArray(books)) {
+      res.status(400).json({ error: 'Must be an array of Books' });
+    }
+  
+    try {
+      const bookStream = Readable.from(books);
+  
+      bookStream
+        .on('data', async (bookData) => {
+          bookStream.pause(); 
+          try {
+            const book = await Book.create({
+              title: bookData.title,
+              author_id: bookData.author_id,
+              isbn: bookData.isbn,
+              publication_date: bookData.publication_date,
+              price: bookData.price,
+              category_id: bookData.category_id, 
+            });
+  
+            eventEmitter.emit('bookCreated', book);
+          } catch (error: any) {
+            console.error('Error processing book:', error.message);
+          }
+          bookStream.resume();
+        })
+        .on('end', () => {
+          res.status(200).json({ message: `${books.length} books processed` });
+        })
+        .on('error', (error) => {
+          res.status(500).json({ error: error.message });
+        });
+    } catch (error: any) {
+      res.status(500).json({ error: error.message });
+    }
+};
+  
--- src/route/bookRoute.ts
+++ src/route/bookRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createBook, deleteBookDetails, getAllBooks, updateBookDetails } from '../controller/bookController.js';
+import { bulkBook, createBook, deleteBookDetails, getAllBooks, updateBookDetails } from '../controller/bookController.js';
 const router= express.Router();
 
 router.post('/create-book',createBook);
 router.get('/getAllBooks',getAllBooks);
 router.put('/update-book/:id',updateBookDetails)
 router.delete('/delete-book/:id',deleteBookDetails);
+router.post('/bulk',bulkBook)
 
 export default router;
